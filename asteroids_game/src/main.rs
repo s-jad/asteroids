3c2144ddb34286f89f4f686d2b1d3092abe0054e
s-jad <s_jad@web.de>
2024-02-26 12:55:51 +0100

    simplified App loop with plugins

--- asteroids_game/src/main.rs
+++ asteroids_game/src/main.rs
@@ -1,11 +1,16 @@
-use bevy::prelude::*;
-use player::spawn_spaceship;
+use bevy::{app::App, DefaultPlugins};
+use player::SpaceshipPlugin;
+use plugins::{DebugPlugin, MovementPlugin};
 mod components;
 mod player;
+mod plugins;
 
 fn main() {
     App::new()
-        .add_systems(Startup, spawn_spaceship)
         .add_plugins(DefaultPlugins)
+        // User configured plugins
+        .add_plugins(SpaceshipPlugin)
+        .add_plugins(MovementPlugin)
+        .add_plugins(DebugPlugin)
         .run();
 }
