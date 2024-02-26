bf9bdf929a0a0eb7fe8b2bbdab434713e839cda4
s-jad <s_jad@web.de>
2024-02-26 14:20:52 +0100

    included time.delta_seconds to make time consistent across diff processor speeds

--- asteroids_game/src/plugins/movement_plugins.rs
+++ asteroids_game/src/plugins/movement_plugins.rs
@@ -1,6 +1,7 @@
 use bevy::{
     app::{App, Plugin, Update},
-    ecs::system::Query,
+    ecs::system::{Query, Res},
+    time::Time,
     transform::components::Transform,
 };
 
@@ -14,18 +15,18 @@ impl Plugin for MovementPlugin {
     }
 }
 
-fn update_position(mut query: Query<(&Velocity, &mut Transform)>) {
+fn update_position(mut query: Query<(&Velocity, &mut Transform)>, time: Res<Time>) {
     for (vel, mut transform) in query.iter_mut() {
-        transform.translation.x += vel.val.x;
-        transform.translation.y += vel.val.y;
-        transform.translation.z += vel.val.z;
+        transform.translation.x += vel.val.x * time.delta_seconds();
+        transform.translation.y += vel.val.y * time.delta_seconds();
+        transform.translation.z += vel.val.z * time.delta_seconds();
     }
 }
 
-fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>) {
+fn update_velocity(mut query: Query<(&Acceleration, &mut Velocity)>, time: Res<Time>) {
     for (acc, mut vel) in query.iter_mut() {
-        vel.val.x += acc.val.x;
-        vel.val.y += acc.val.y;
-        vel.val.z += acc.val.z;
+        vel.val.x += acc.val.x * time.delta_seconds();
+        vel.val.y += acc.val.y * time.delta_seconds();
+        vel.val.z += acc.val.z * time.delta_seconds();
     }
 }
