d5d854b7e944acc34af7731b669a05b5acb1c303
s-jad <s_jad@web.de>
2024-02-27 17:07:06 +0100

    Delegated asset loading to AssetLoaderPlugin

--- asteroids_game/src/player/player.rs
+++ asteroids_game/src/player/player.rs
@@ -1,5 +1,16 @@
-use crate::components::{Acceleration, MovingObjectBundle, Velocity};
-use bevy::prelude::*;
+use bevy::{
+    app::{App, Plugin, PostStartup},
+    ecs::system::{Commands, Res},
+    math::Vec3,
+    prelude::default,
+    scene::SceneBundle,
+    transform::components::Transform,
+};
+
+use crate::{
+    components::{Acceleration, MovingObjectBundle, Velocity},
+    plugins::asset_loader::SceneAssets,
+};
 
 const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 0.1);
 const STARTING_ACCELERATION: Vec3 = Vec3::ZERO;
@@ -9,18 +20,18 @@ pub struct SpaceshipPlugin;
 
 impl Plugin for SpaceshipPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_spaceship);
+        app.add_systems(PostStartup, spawn_spaceship);
     }
 }
 
-fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
+fn spawn_spaceship(mut commands: Commands, asset_server: Res<SceneAssets>) {
     let scale = Vec3::new(0.5, 0.5, 0.5);
 
     commands.spawn(MovingObjectBundle {
         velocity: Velocity::new(STARTING_VELOCITY),
         acceleration: Acceleration::new(STARTING_ACCELERATION),
         model: SceneBundle {
-            scene: asset_server.load("player_spaceship.glb#Scene0"),
+            scene: asset_server.player_spaceship.clone(),
             transform: Transform {
                 translation: STARTING_TRANSLATION,
                 scale,
