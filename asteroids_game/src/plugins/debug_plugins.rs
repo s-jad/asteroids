b29cf73153fa2f29b909845b2da991a4d715b43a
s-jad <s_jad@web.de>
2024-02-27 15:06:12 +0100

    Added timer (2s) to debug info to make it more readable

--- asteroids_game/src/plugins/debug_plugins.rs
+++ asteroids_game/src/plugins/debug_plugins.rs
@@ -1,29 +1,50 @@
+use crate::components::Velocity;
 use bevy::{
     app::{Plugin, Update},
-    ecs::{entity::Entity, system::Query},
+    ecs::{
+        entity::Entity,
+        system::{Query, Res, ResMut, Resource},
+    },
     log::info,
     prelude::App,
+    time::{Time, Timer, TimerMode},
     transform::components::Transform,
 };
 
-use crate::components::Velocity;
+#[derive(Debug, Resource)]
+struct DebugTimer {
+    time: Timer,
+}
+
+impl Default for DebugTimer {
+    fn default() -> Self {
+        Self {
+            time: Timer::from_seconds(2.0, TimerMode::Repeating),
+        }
+    }
+}
 
 pub struct DebugPlugin;
 
 impl Plugin for DebugPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, (print_position, print_velocity));
+        app.insert_resource(DebugTimer::default())
+            .add_systems(Update, (print_position, print_velocity));
     }
 }
 
-fn print_position(query: Query<(Entity, &Transform)>) {
-    for (entity, transform) in query.iter() {
-        info!("Entity[{:?}] moved to {:?}", entity, transform);
+fn print_position(query: Query<(Entity, &Transform)>, mut dt: ResMut<DebugTimer>, time: Res<Time>) {
+    if dt.time.tick(time.delta()).just_finished() {
+        for (entity, transform) in query.iter() {
+            info!("Entity[{:?}] moved to {:?}", entity, transform);
+        }
     }
 }
 
-fn print_velocity(query: Query<(Entity, &Velocity)>) {
-    for (entity, velocity) in query.iter() {
-        info!("Entity[{:?}] velocity: {:?}", entity, velocity);
+fn print_velocity(query: Query<(Entity, &Velocity)>, mut dt: ResMut<DebugTimer>, time: Res<Time>) {
+    if dt.time.tick(time.delta()).just_finished() {
+        for (entity, vel) in query.iter() {
+            info!("Entity[{:?}] moved to {:?}", entity, vel);
+        }
     }
 }
