7f654e57a25a1592428d08633f5d6ef81b79b374
s-jad <s_jad@web.de>
2024-02-27 15:05:39 +0100

    Moved spaceship movement properties to MovingObjectBundle, scaled ship

--- asteroids_game/src/player/player.rs
+++ asteroids_game/src/player/player.rs
@@ -1,16 +1,9 @@
-use crate::components::{Acceleration, Velocity};
+use crate::components::{Acceleration, MovingObjectBundle, Velocity};
 use bevy::prelude::*;
 
-const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 1.0);
-const STARTING_ACCELERATION: Vec3 = Vec3::new(0.0, 0.0, 0.0);
+const STARTING_VELOCITY: Vec3 = Vec3::new(0.0, 0.0, 0.1);
+const STARTING_ACCELERATION: Vec3 = Vec3::ZERO;
 const STARTING_TRANSLATION: Vec3 = Vec3::new(0.0, 0.0, -20.0);
-
-#[derive(Bundle)]
-struct SpaceshipBundle {
-    velocity: Velocity,
-    acceleration: Acceleration,
-    model: SceneBundle,
-}
 
 pub struct SpaceshipPlugin;
 
@@ -21,12 +14,18 @@ impl Plugin for SpaceshipPlugin {
 }
 
 fn spawn_spaceship(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(SpaceshipBundle {
+    let scale = Vec3::new(0.5, 0.5, 0.5);
+
+    commands.spawn(MovingObjectBundle {
         velocity: Velocity::new(STARTING_VELOCITY),
         acceleration: Acceleration::new(STARTING_ACCELERATION),
         model: SceneBundle {
             scene: asset_server.load("player_spaceship.glb#Scene0"),
-            transform: Transform::from_translation(STARTING_TRANSLATION),
+            transform: Transform {
+                translation: STARTING_TRANSLATION,
+                scale,
+                ..default()
+            },
             ..default()
         },
     });
