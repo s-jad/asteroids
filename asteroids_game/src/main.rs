b2d37a6a8d967d6cbf3f1a86fed39ce0799b161b
s-jad <s_jad@web.de>
2024-02-27 15:04:59 +0100

    Moved lighting to own module, added EnemyPlugin to spawn asteroids

--- asteroids_game/src/main.rs
+++ asteroids_game/src/main.rs
@@ -1,31 +1,25 @@
 mod camera;
 mod components;
+mod enemies;
 mod player;
 mod plugins;
 
-use bevy::{
-    app::App,
-    pbr::AmbientLight,
-    render::{camera::ClearColor, color::Color},
-    DefaultPlugins,
-};
+use bevy::{app::App, DefaultPlugins};
 
 use camera::CameraPlugin;
+use enemies::EnemyPlugin;
 use player::SpaceshipPlugin;
-use plugins::{DebugPlugin, MovementPlugin};
+use plugins::{DebugPlugin, LightingPlugin, MovementPlugin};
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.25, 0.0, 0.15)))
-        .insert_resource(AmbientLight {
-            color: Color::default(),
-            brightness: 125.0,
-        })
         .add_plugins(DefaultPlugins)
         // User configured plugins
+        .add_plugins(LightingPlugin)
         .add_plugins(MovementPlugin)
-        .add_plugins(DebugPlugin)
         .add_plugins(SpaceshipPlugin)
+        .add_plugins(EnemyPlugin)
         .add_plugins(CameraPlugin)
+        .add_plugins(DebugPlugin)
         .run();
 }
