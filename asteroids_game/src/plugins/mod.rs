920ae8ac37acf2412444d1cccddf5f2cb1b77c33
s-jad <s_jad@web.de>
2024-02-27 15:06:39 +0100

    lighting_plugins mod added

--- asteroids_game/src/plugins/mod.rs
+++ asteroids_game/src/plugins/mod.rs
@@ -1,5 +1,7 @@
 pub mod debug_plugins;
+pub mod lighting_plugins;
 pub mod movement_plugins;
 
 pub use debug_plugins::DebugPlugin;
+pub use lighting_plugins::LightingPlugin;
 pub use movement_plugins::MovementPlugin;
